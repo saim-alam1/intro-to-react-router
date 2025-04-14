34a613a3b5e2bc197578e0667517227189d96bb2
saim-alam1 <allbehassansaim@gmail.com>
2025-04-15 02:37:50 +0600

    Implementing Routed Components

---
+++ src/components/Mobiles/Mobiles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mobiles = () => {
+  return (
+    <div>
+      <h2>This is mobiles component</h2>
+    </div>
+  );
+};
+
+export default Mobiles;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,37 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Mobiles from './components/Mobiles/Mobiles.jsx';
+import Laptops from './components/Laptops/Laptops.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello from react router</div>
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: 'mobiles', Component: Mobiles },
+      { path: 'laptops', Component: Laptops }
+    ]
+  },
+  {
+    path: 'about',
+    element: <div>About Me Here</div>
+  },
+  {
+    path: 'blogs',
+    element: <div>All My Blogs Are Here</div>
+  },
+  {
+    path: 'app',
+    Component: App,
+  },
+  {
+    path: '/app2',
+    // element: <App></App>,
+    Component: App,
   }
 ])
 
