6ce89e6b6b172843c49a763dc87386c884ca537e
saim-alam1 <allbehassansaim@gmail.com>
2025-04-16 12:55:42 +0600

    Loaded Posts Data

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UserDetails = () => {
+  const user = useLoaderData();
+  const { name, website } = user;
   return (
     <div>
       <h3>User Details Here</h3>
+      <h5>Name: {name}</h5>
+      <p>Website: {website}</p>
     </div>
   );
 };
